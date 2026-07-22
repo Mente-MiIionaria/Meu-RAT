30c083501810dcc281568a1ca76432139a02c470
Milionario <kleytonvelichane3@gmail.com>
2026-07-23 01:36:24 +0200

    Simplificar mensagens do System.out.println para melhor compreensão

--- ExameNormal.java
+++ ExameNormal.java
@@ -22,22 +22,21 @@ public class ExameNormal
 		
 		do {
 			
-			System.out.println ("\nBem vindo a Lindor!");
+			System.out.println ("\n*** Bem-vindo a Lindor! ***");
 			
-			System.out.println ("\n=================================================================================");
-			System.out.println ("||--------------------------------------MENU-----------------------------------||");
-			System.out.println ("|| 1-Receber os dados e calcular o valor a pagar                               ||");
-			System.out.println ("|| 2-Visualizar o valor total de cada tipo                                     ||");
-			System.out.println ("|| 3-Visualizar a quantidade total de cada tipo                                ||");
-			System.out.println ("|| 4-Visualizar a quantidade de clientes que pediu caramelo                    ||");
-			System.out.println ("|| 6-Visualizar o valor total de descontos                                     ||");
-			System.out.println ("|| 5-Visualizar o valor total recebido pela empresa com iva                    ||");
-			System.out.println ("|| 7-Verificar qual dos chocolates amargos e mais vendida                      ||");
-			System.out.println ("|| 8-Verificar se a empresa tera valor suficiente para pagar a renda           ||");
-			System.out.println ("|| 9-Sair do programa                                                          ||");
-			System.out.println ("||=============================================================================||");
-			System.out.println ("\nIntoduza a opcao desejada (1-7)");
-			System.out.print ("opcao ->");opcao = Integer.parseInt(br.readLine () );
+			System.out.println ("\n===== MENU PRINCIPAL =====");
+			System.out.println ("1. Fazer pedido");
+			System.out.println ("2. Ver valor total por tipo");
+			System.out.println ("3. Ver quantidade vendida por tipo");
+			System.out.println ("4. Ver clientes com caramelo");
+			System.out.println ("5. Ver descontos totais");
+			System.out.println ("6. Ver receita total (com IVA)");
+			System.out.println ("7. Tipo amargo mais vendido");
+			System.out.println ("8. Verificar renda");
+			System.out.println ("9. Sair");
+			System.out.println ("===========================");
+			System.out.print ("Escolha uma opcao (1-9): ");
+			opcao = Integer.parseInt(br.readLine () );
 			
 			switch (opcao) 
 			{
@@ -50,11 +49,11 @@ public class ExameNormal
 						do 
 						{
 							
-							System.out.println ("Introduza o tipo de chocolate (L-Leite / A-Amargo / E-Escuro)");
+							System.out.print ("Tipo de chocolate (L/A/E): ");
 							tpC = br.readLine ().charAt(0);
 							
 							if (tpC != 'L' && tpC != 'A' && tpC != 'E')
-								System.out.println ("ERROR, tente novamente!");
+								System.out.println ("Invalido! Digite L, A ou E");
 							
 						}
 						while (tpC != 'L' && tpC != 'A' && tpC != 'E');
@@ -62,11 +61,11 @@ public class ExameNormal
 						do 
 						{
 						   
-							System.out.println ("Introduza a quantidade de clientes (>0)");
+							System.out.print ("Quantidade (>0): ");
 							quant = Integer.parseInt (br.readLine ());
 							
 							if (quant < 0)
-								System.out.println ("ERROR, tente novamente!");
+								System.out.println ("Quantidade deve ser positiva!");
 				
 						}
 						while (quant<0);
@@ -81,11 +80,11 @@ public class ExameNormal
 									do 
 									{
 										
-										System.out.println ("Deseja molho de caramelo? (S-Sim / N-Nao)");
+										System.out.print ("Quer caramelo? (S/N): ");
 										molhoC = br.readLine ().charAt(0);
 										
 										if (molhoC != 'S' && molhoC != 'N')
-											System.out.println ("ERROR, tente novamente!");
+											System.out.println ("Digite S ou N");
 										
 									}
 									while (molhoC != 'S' && molhoC != 'N');
@@ -104,11 +103,11 @@ public class ExameNormal
 									do 
 									{
 										
-										System.out.println ("Qual e o nivel de acucar que deseja (0 / 2)");
+										System.out.print ("Nivel de acucar (0/2): ");
 										nivelacucar = Integer.parseInt(br.readLine ());
 										
 										if (nivelacucar != 0 && nivelacucar != 2)
-											System.out.println ("ERROR, tente novamente!");
+											System.out.println ("Digite 0 ou 2");
 										
 									}
 									while (nivelacucar != 0 && nivelacucar != 2);
@@ -139,11 +138,11 @@ public class ExameNormal
 									do 
 									{
 										
-										System.out.println ("Introduza a percentagem de cacau (30-90%)");
+										System.out.print ("Percentagem de cacau (30-90): ");
 										cacau = Integer.parseInt(br.readLine ());
 										
 										if (cacau < 30 || cacau > 90)
-											System.out.println ("ERROR, tente novamente!");
+											System.out.println ("Digite entre 30 e 90");
 										
 									}
 									while (cacau < 30 || cacau > 90);
@@ -166,56 +165,56 @@ public class ExameNormal
 						valorfinal = valorpagar + iva;
 						vTempresa += valorfinal;
 						
-						System.out.println ("O valor final a pagar e " +valorfinal);
+						System.out.println (">> Valor a pagar: " +valorfinal+ " MTs");
 					
 					}
 					
 		
 				break;
 				
-				case 2: System.out.println ("Valores totais de cada tipo:");
-				        System.out.println ("Chocolate leite " +mt.format(vTL));
-				        System.out.println ("Chocolate amargo " +mt.format(vTA));
-				        System.out.println ("Chocolate escuro " +mt.format(vTE));
+				case 2: System.out.println ("\n--- Valores Totais ---");
+				        System.out.println ("Leite: " +mt.format(vTL));
+				        System.out.println ("Amargo: " +mt.format(vTA));
+				        System.out.println ("Escuro: " +mt.format(vTE));
 				break;
 				
-				case 3: System.out.println ("Quantidade total vendida de cada tipo");
-						System.out.println ("Chocolate leite " +quantL);
-				        System.out.println ("Chocolate amargo " +quantA);
-				        System.out.println ("Chocolate escuro " +quantE);
+				case 3: System.out.println ("\n--- Quantidade Vendida ---");
+						System.out.println ("Leite: " +quantL);
+				        System.out.println ("Amargo: " +quantA);
+				        System.out.println ("Escuro: " +quantE);
 				break;
 				
-				case 4: System.out.println ("A quantidade de cliente que pediu caramelo e " +contm);
+				case 4: System.out.println ("\nClientes com caramelo: " +contm);
 			    break;
 			    
-				case 5: System.out.println ("O valor total de desconto e " +mt.format(vTd));
+				case 5: System.out.println ("\nDescontos totais: " +mt.format(vTd));
 				break;
 				
-				case 6: System.out.println ("O valor total recebido da empresa com iva e " +mt.format(vTempresa));
+				case 6: System.out.println ("\nReceita com IVA: " +mt.format(vTempresa));
 				break;
 				
 				case 7: 
 					if (cont0 > cont2)	
-					   System.out.println ("O nivel de acucar 0 e o mais vendido");
+					   System.out.println ("\nNivel 0 mais vendido");
 					else
 						if (cont2 > cont0)
-							System.out.println ("O nivel de acucar 2 e o mais vendido");
+							System.out.println ("\nNivel 2 mais vendido");
 					    else 
-					    	System.out.println ("O nivel de acucar 0 e 2 sao os mais vendidos");
+					    	System.out.println ("\nNiveis 0 e 2 igual");
 							
 				break;
 				
 				case 8: 
 					if (valorsemiva > RENDA)
-					   System.out.println ("O valor da empresa e suficiente para pagar a renda");
+					   System.out.println ("\nRenda OK");
 					else
-						System.out.println ("O valor da empresa nao e suficiente para pagar a renda");
+						System.out.println ("\nRenda insuficiente");
 					
 					    valoremfalta = RENDA - valorsemiva;
-					    System.out.println ("O valor em falta para cubrir a renda e " +usd.format(valoremfalta));
+					    System.out.println ("Falta: " +usd.format(valoremfalta));
 				break;
 				
-				case 9: System.out.println ("Muito obrigado por usar os nossos servicos");
+				case 9: System.out.println ("\nAte logo!");
 				break;
 				
 				
